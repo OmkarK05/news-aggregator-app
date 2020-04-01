d2569d099616894ed6dd16247bcf1f22c910f95f
omkar Kesarkhane <omkesarkhane@gmail.com>
2020-04-01 20:17:53 +0530

    updated errors

--- src/scripts/scripts2/search.js
+++ src/scripts/scripts2/search.js
@@ -10,7 +10,7 @@ export class TopNews{
     }
     catch(error)
     {
-        console.log("Error" + error);
+        alert(error);
     }
  }
     
@@ -21,9 +21,15 @@ constructor(query){
     this.query = query;
 }
         async getSearchResults(query){
+        try{
         const news = await fetch(`https://newsapi.org/v2/top-headlines?q=${this.query}&apiKey=02fbe3d555cf4e3483390b73a7a7bb24`);
         const res = await news.json();
         this.results = res.articles;
+        }
+        catch(error)
+        {
+            alert(error);
+        }
    }
 
 }
